d72c8ee1da119a573850d16d592289e94384a822
egeres <egeres.default@gmail.com>
2023-12-03 00:18:47 +0100

    feat: Tweaked model params for better quality

--- model.py
+++ model.py
@@ -16,11 +16,18 @@ logger.add("model_runs.log", rotation="1 MB")
 class Model(ABC):
     url: str
 
+    def generate_cached(
+        self,
+    ):
+        # TODO: Implement this in the future
+        pass
+
     def generate(
         self,
         prompt: str,
+        negative_prompt: str = "mutilated, poorly lit, bad shadow, draft, cropped, out of frame, cut off, censored, jpeg artifacts, out of focus, glitch, duplicate, (worst quality, low quality, normal quality, lowres, low details, oversaturated, undersaturated, overexposed, underexposed, grayscale, bw, bad photo, bad photography, bad art:1.4), (watermark, signature, text font, username, error, logo, words, letters, digits, autograph, trademark, name:1.2), (blur, blurry, grainy), (bad hands, bad anatomy, bad body, bad face, bad teeth, bad arms, bad legs, deformities:1.3)",
         number_of_imgs: int = 1,
-        steps: int = 20,
+        steps: int = 35,
         cfg_scale: float = 7.0,
         size: tuple[int, int] = (512, 512),
     ) -> list[Path]:
@@ -33,11 +40,15 @@ class Model(ABC):
         assert isinstance(cfg_scale, float), "cfg_scale must be a float"
         assert isinstance(size, tuple), "size must be a tuple"
 
+        # TODO: Add arg "batch_size": 1,
+
         return [
             self._generate(
                 prompt=prompt,
+                negative_prompt=negative_prompt,
                 steps=steps,
                 cfg_scale=cfg_scale,
+                size=size,
             )
             for _ in range(number_of_imgs)
         ]
@@ -46,7 +57,8 @@ class Model(ABC):
     def _generate(
         self,
         prompt: str,
-        steps: int = 20,
+        negative_prompt: str = "",
+        steps: int = 35,
         cfg_scale: float = 7.0,
         size: tuple[int, int] = (512, 512),
     ) -> Path:
@@ -75,7 +87,8 @@ class Model_SD_0(Model):
     def _generate(
         self,
         prompt: str,
-        steps: int = 20,
+        negative_prompt: str = "mutilated, poorly lit, bad shadow, draft, cropped, out of frame, cut off, censored, jpeg artifacts, out of focus, glitch, duplicate, (worst quality, low quality, normal quality, lowres, low details, oversaturated, undersaturated, overexposed, underexposed, grayscale, bw, bad photo, bad photography, bad art:1.4), (watermark, signature, text font, username, error, logo, words, letters, digits, autograph, trademark, name:1.2), (blur, blurry, grainy), (bad hands, bad anatomy, bad body, bad face, bad teeth, bad arms, bad legs, deformities:1.3)",
+        steps: int = 35,
         cfg_scale: float = 7.0,
         size: tuple[int, int] = (512, 512),
     ) -> Path:
@@ -85,21 +98,27 @@ class Model_SD_0(Model):
             url=f"{self.url}/sdapi/v1/txt2img",
             json={
                 "prompt": prompt,
+                "negative_prompt": negative_prompt,
                 "steps": steps,
                 "cfg_scale": cfg_scale,
                 "width": size[0],
                 "height": size[1],
+                "sampler_name": "DPM++ 2M Karras",
             },
         )
-        assert response.status_code == 200, "Server error"
+        assert response.status_code == 200, f"Server error: {response.text}"
         r = response.json()
         image = Image.open(io.BytesIO(base64.b64decode(r["images"][0])))
-        path = Path(f"outputs/{datetime.datetime.now()}_{prompt}.png")
+
+        date_format = "%Y-%m-%d_%H-%M-%S"
+        date_formatted = datetime.datetime.now().strftime(date_format)
+        path = Path(f"outputs/{date_formatted}_{prompt}.png")
         image.save(path)
         return path
 
 
 if __name__ == "__main__":
     m = Model_SD_0()
-    # o = m.generate(prompt="a nurse", number_of_imgs=3)
-    o = m.get_models()
+    o = m.generate(prompt="a nurse", number_of_imgs=3)
+    # o = m.get_models()
+    p = 0
