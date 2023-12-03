64c9f91eb464d26a45a58af5c2271e7107bb04d0
Martin Sanchez Pueyo <67611933+mspueyo@users.noreply.github.com>
2023-12-03 17:43:32 +0100

    add arguments --generate (-g) and --evaluate (-e) to main

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import argparse
 import os
 
-from batch_evaluator import BatchEvaluator
+from evaluator import Evaluator
 from model import Model_SD_0
 from utils.config import read_config
 from utils.logger import logger
 
 root_dir = os.path.dirname(__file__)
+
 config = read_config("config/config.ini")
 
 
@@ -15,12 +17,27 @@ def main():
     project_name = config.get("tool.poetry", "name").replace('"', "")
     logger.info(f"Starting {project_name}")
 
-    # Model & evaluator instantiation
-    model = Model_SD_0()
-    evaluator = BatchEvaluator()
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-g', '--generate', action='store_true')
+    parser.add_argument('-e', '--evaluate', action='store_true')
+    args = parser.parse_args()
 
-    # Actual evaluation
-    eval = evaluator.execute()
+    generate = args.generate
+    evaluate = args.evaluate
+    if not (generate or evaluate):
+        generate = True
+        evaluate = True
+
+    # Model and image generation
+    if generate:
+        model = Model_SD_0()
+        images = model.generate_batch()
+
+    # Evaluation
+    if evaluate:
+        evaluator = Evaluator()
+        eval = evaluator.execute_batch()
 
 
 if __name__ == "__main__":
