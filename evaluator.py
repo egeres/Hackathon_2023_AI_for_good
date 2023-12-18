19810d4d8f6b7601cdad23736338d1a57f4717e6
egeres <egeres.default@gmail.com>
2023-12-18 17:24:23 +0100

    fix: Adjusted model's default params to work with juggernautXL

--- evaluator.py
+++ evaluator.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import os
 from math import log2
 from pathlib import Path
 
--- model.py
+++ model.py
@@ -28,7 +28,7 @@ class Model(ABC):
     def generate(
         self,
         prompt: str,
-        negative_prompt: str = "mutilated, poorly lit, bad shadow, draft, cropped, out of frame, cut off, censored, jpeg artifacts, out of focus, glitch, duplicate, (worst quality, low quality, normal quality, lowres, low details, oversaturated, undersaturated, overexposed, underexposed, grayscale, bw, bad photo, bad photography, bad art:1.4), (watermark, signature, text font, username, error, logo, words, letters, digits, autograph, trademark, name:1.2), (blur, blurry, grainy), (bad hands, bad anatomy, bad body, bad face, bad teeth, bad arms, bad legs, deformities:1.3)",
+        negative_prompt: str = "(worst quality, low quality, normal quality, lowres, low details, oversaturated, undersaturated, overexposed, underexposed, grayscale, bw, bad photo, bad photography, bad art:1.4), (watermark, signature, text font, username, error, logo, words, letters, digits, autograph, trademark, name:1.2), (blur, blurry, grainy), morbid, ugly, asymmetrical, mutated malformed, mutilated, poorly lit, bad shadow, draft, cropped, out of frame, cut off, censored, jpeg artifacts, out of focus, glitch, duplicate, (airbrushed, cartoon, anime, semi-realistic, cgi, render, blender, digital art, manga, amateur:1.3), (3D ,3D Game, 3D Game Scene, 3D Character:1.1), (bad hands, bad anatomy, bad body, bad face, bad teeth, bad arms, bad legs, deformities:1.3)",
         number_of_imgs: int = 1,
         steps: int = 35,
         cfg_scale: float = 7.0,
@@ -52,7 +52,7 @@ class Model(ABC):
 
         if cache:  # Actually, we assume this is the usual case!
             hash_prompt = hex_hash(prompt)
-            path = Path(f"outputs/{hash_prompt}")
+            path = Path(__file__).parent / "outputs" / hash_prompt
             if not path.exists():
                 path.mkdir(parents=True)
 
@@ -99,6 +99,7 @@ class Model(ABC):
         cfg_scale: float = 7.0,
         size: tuple[int, int] = (512, 512),
         path_dir_output: Path | None = None,
+        batch_size: int = 1,
     ) -> Path:
         """Generates an image from a prompt."""
 
@@ -142,11 +143,12 @@ class Model_SD_0(Model):
     def _generate(
         self,
         prompt: str,
-        negative_prompt: str = "mutilated, poorly lit, bad shadow, draft, cropped, out of frame, cut off, censored, jpeg artifacts, out of focus, glitch, duplicate, (worst quality, low quality, normal quality, lowres, low details, oversaturated, undersaturated, overexposed, underexposed, grayscale, bw, bad photo, bad photography, bad art:1.4), (watermark, signature, text font, username, error, logo, words, letters, digits, autograph, trademark, name:1.2), (blur, blurry, grainy), (bad hands, bad anatomy, bad body, bad face, bad teeth, bad arms, bad legs, deformities:1.3)",
+        negative_prompt: str = "(worst quality, low quality, normal quality, lowres, low details, oversaturated, undersaturated, overexposed, underexposed, grayscale, bw, bad photo, bad photography, bad art:1.4), (watermark, signature, text font, username, error, logo, words, letters, digits, autograph, trademark, name:1.2), (blur, blurry, grainy), morbid, ugly, asymmetrical, mutated malformed, mutilated, poorly lit, bad shadow, draft, cropped, out of frame, cut off, censored, jpeg artifacts, out of focus, glitch, duplicate, (airbrushed, cartoon, anime, semi-realistic, cgi, render, blender, digital art, manga, amateur:1.3), (3D ,3D Game, 3D Game Scene, 3D Character:1.1), (bad hands, bad anatomy, bad body, bad face, bad teeth, bad arms, bad legs, deformities:1.3)",
         steps: int = 35,
         cfg_scale: float = 7.0,
-        size: tuple[int, int] = (512, 512),
+        size: tuple[int, int] = (768, 1168),
         path_dir_output: Path | None = None,
+        batch_size: int = 1,
     ) -> list[Path]:
         logger.info(f"Request: {prompt}, {steps}, {cfg_scale}, {size}")
 
@@ -158,14 +160,15 @@ class Model_SD_0(Model):
             url=f"{self.url}/sdapi/v1/txt2img",
             json={
                 # "prompt": prompt + ", photorealistic, soft light, high quality",
-                "prompt": prompt,
-                # "negative_prompt": negative_prompt,
+                "prompt": prompt + ", photorealistic, soft light",
+                "negative_prompt": negative_prompt,
+                "sampler_name": "DPM++ 2M Karras",
                 "steps": steps,
                 "cfg_scale": cfg_scale,
                 "width": size[0],
                 "height": size[1],
-                "sampler_name": "DPM++ 2M Karras",
-                "batch_size": 4,
+                "batch_size": batch_size,
+                "seed": 1,
             },
         )
         assert response.status_code == 200, f"Server error: {response.text}"
@@ -185,4 +188,7 @@ class Model_SD_0(Model):
 
 if __name__ == "__main__":
     m = Model_SD_0()
-    Model_SD_0().generate_batch()
+    # Model_SD_0().generate_batch()
+    m.generate(
+        "a nurse",
+    )
