c43a699582dab8cb1460a2b6d74c308aa40fce89
egeres <egeres.default@gmail.com>
2023-12-28 18:21:06 +0100

    refactor: Tweaked image generation and added more sample data

--- model.py
+++ model.py
@@ -24,15 +24,16 @@ def hex_hash(s: str) -> str:
 
 class Model(ABC):
     url: str
+    default_pics_dir: Path = Path(__file__).parent / "generated_images"
 
     def generate(
         self,
         prompt: str,
         negative_prompt: str = "(worst quality, low quality, normal quality, lowres, low details, oversaturated, undersaturated, overexposed, underexposed, grayscale, bw, bad photo, bad photography, bad art:1.4), (watermark, signature, text font, username, error, logo, words, letters, digits, autograph, trademark, name:1.2), (blur, blurry, grainy), morbid, ugly, asymmetrical, mutated malformed, mutilated, poorly lit, bad shadow, draft, cropped, out of frame, cut off, censored, jpeg artifacts, out of focus, glitch, duplicate, (airbrushed, cartoon, anime, semi-realistic, cgi, render, blender, digital art, manga, amateur:1.3), (3D ,3D Game, 3D Game Scene, 3D Character:1.1), (bad hands, bad anatomy, bad body, bad face, bad teeth, bad arms, bad legs, deformities:1.3)",
-        number_of_imgs: int = 1,
         steps: int = 35,
         cfg_scale: float = 7.0,
-        size: tuple[int, int] = (512, 512),
+        size: tuple[int, int] = (768, 1168),
+        number_of_imgs: int = 1,
         cache: bool = True,
     ) -> list[Path]:
         """Generates various images from a prompt.
@@ -52,7 +53,7 @@ class Model(ABC):
 
         if cache:  # Actually, we assume this is the usual case!
             hash_prompt = hex_hash(prompt)
-            path = Path(__file__).parent / "outputs" / hash_prompt
+            path = self.default_pics_dir / hash_prompt
             if not path.exists():
                 path.mkdir(parents=True)
 
@@ -104,9 +105,9 @@ class Model(ABC):
 
     def generate_batch(
         self,
-        prompt_list: list[str] = None,
-        n_images: int = None,
-        iterations: int = None,
+        prompt_list: list[str] | None = None,
+        n_images: int | None = None,
+        iterations: int | None = None,
     ):
         if prompt_list is None:
             prompt_list = config.get("MODEL", "prompts").split(",")
@@ -144,13 +145,10 @@ class Model_SD_0(Model):
         steps: int = 35,
         cfg_scale: float = 7.0,
         size: tuple[int, int] = (768, 1168),
-        path_dir_output: Path | None = None,
+        path_dir_output: Path = Model.default_pics_dir,
         batch_size: int = 1,
     ) -> list[Path]:
         logger.info(f"Request: {prompt}, {steps}, {cfg_scale}, {size}")
-
-        if path_dir_output is None:
-            path_dir_output = Path("outputs")
 
         # Request itself
         response = requests.post(
@@ -165,7 +163,7 @@ class Model_SD_0(Model):
                 "width": size[0],
                 "height": size[1],
                 "batch_size": batch_size,
-                "seed": 1,
+                # "seed": 1,
             },
         )
         assert response.status_code == 200, f"Server error: {response.text}"
@@ -186,6 +184,5 @@ class Model_SD_0(Model):
 if __name__ == "__main__":
     m = Model_SD_0()
     # Model_SD_0().generate_batch()
-    m.generate(
-        "a nurse",
-    )
+    m.generate("a nurse", number_of_imgs=10, cache=True)
+    m.generate("a doctor", number_of_imgs=10, cache=True)
