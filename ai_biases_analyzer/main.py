30469f1a03ce5bdd20ed30a0566f9683cbfe5436
mspueyo <67611933+mspueyo@users.noreply.github.com>
2024-01-07 19:25:41 +0100

    feat: parametrize generator options

--- ai_biases_analyzer/main.py
+++ ai_biases_analyzer/main.py
@@ -17,21 +17,35 @@ config = read_config("config/config.ini")
 
 
 @app.command(help="Run the end-to-end pipeline, generating images and evaluating them.")
-def run(prompts: list[str] | None = None):
-    generate(prompts)
+def run(
+    prompts: list[str] | None = None,
+    n_images: int | None = None,
+    iterations: int | None = None,
+):
+    generate(prompts, n_images, iterations)
     evaluate(prompts)
 
 
 @app.command(help="Generate images from prompts.")
-def generate(prompts: list[str] | None = None):
+def generate(
+    prompts: list[str] | None = None,
+    n_images: int | None = None,
+    iterations: int | None = None,
+):
     if prompts is None:
         config.get("DEFAULT", "prompts").split(",")
+    if n_images is None:
+        n_images = int(config.get("DEFAULT", "n_images"))
+    if iterations is None:
+        iterations = int(config.get("DEFAULT", "iterations"))
     model = Model_SD_0()
-    model.generate_batch(prompts)
+    model.generate_batch(prompts, n_images, iterations)
 
 
 @app.command(help="Evaluates images.")
-def evaluate(prompts: list[str] | None = None):
+def evaluate(prompts: list[str]):
+    if prompts is None:
+        config.get("DEFAULT", "prompts").split(",")
     evaluator = Evaluator(prompts=prompts)
     eval = evaluator.execute_batch()
     logger.info(json.dumps(eval, indent=2))
