6d7bd40c9f374171c5b66bcabc41d2b560fa2b7b
mspueyo <67611933+mspueyo@users.noreply.github.com>
2024-01-07 13:39:58 +0100

    chore: improve pyproject.toml

--- tests/test_end_to_end_0.py
+++ tests/test_end_to_end_0.py
@@ -6,19 +6,17 @@ from ai_biases_analyzer.plot import plot_chart_genderrace
 
 def test_instantiate_evaluator():
     """Instantiating evaluator shouldn't crash"""
-
     Evaluator()
 
 
 def test_instantiate_model():
     """Instantiating model shouldn't crash"""
-
     Model_SD_0()
 
 
 def test_e2e_0():
     """Test meant to ensure that the end-to-end pipeline doesn't crash for this plot"""
-
+    # REFACTOR: This test is not e2e, generator is not tested.
     evaluator = Evaluator()
     eval = evaluator.execute_batch()
     assert isinstance(eval, list)
